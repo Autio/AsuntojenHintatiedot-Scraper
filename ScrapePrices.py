1e2cb43f88f8f8bafae54dcda5cc9e5cf5b736de
Autio <petri.mikael.autio@gmail.com>
2020-06-11 23:58:53 +0300

    Adding Selenium

--- ScrapePrices.py
+++ ScrapePrices.py
@@ -4,22 +4,51 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-r = requests.get("https://asuntojen.hintatiedot.fi/haku/?c=Akaa&cr=1&ps=37800&ps=37830&ps=37900&ps=37910&amin=&amax=&renderType=renderTypeTable&print=1&search=1&submit=Tulosta")
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+d = webdriver.Chrome(options=chrome_options)
+d.get('https://www.google.nl/')
+a = 1
+print(d)
+def get_table():
+    # Need to know the Name, postcode, and then increment the page number
+    # Should also append the postcode to the row to keep track of it
+    # This implies we should only make one postcode query at a time
+    # Could also append the date the code is run
 
-soup = BeautifulSoup(r.text, 'html.parser')
-#print(soup.prettify())
-table = soup.select("#mainTable")
-#print(table)
-#print(table[0])
-output_rows = []
-for table_row in table[0].findAll('tr'):
-    columns = table_row.findAll('td')
-    output_row = []
-    for column in columns:
-        output_row.append(column.text)
-    output_rows.append(output_row)
-with open('output.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerows(output_rows)
-x = 1
+
+    r = requests.get(
+        "https://asuntojen.hintatiedot.fi/haku/?c=Akaa&cr=1&ps=37800&ps=37830&ps=37900&ps=37910&t=4&l=0&z=1&search=1&sf=0&so=a&renderType=renderTypeTable&submit=%C2%AB+edellinen+sivu")
+    soup = BeautifulSoup(r.text, 'html.parser')
+    print(soup.prettify())
+    table = soup.select("#mainTable")
+    return table
+
+
+def get_print_table():
+    r = requests.get(
+        "https://asuntojen.hintatiedot.fi/haku/?c=Akaa&cr=1&ps=37800&ps=37830&ps=37900&ps=37910&amin=&amax=&renderType=renderTypeTable&print=1&search=1&submit=Tulosta")
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    # print(soup.prettify())
+    table = soup.select("#mainTable")
+
+def scrape_data():
+    table = get_table()
+
+    output_rows = []
+    for table_row in table[0].findAll('tr'):
+        columns = table_row.findAll('td')
+        output_row = []
+        for column in columns:
+            output_row.append(column.text)
+        output_rows.append(output_row)
+    with open('output.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(output_rows)
+    x = 1
